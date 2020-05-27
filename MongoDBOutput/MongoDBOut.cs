f981dd31a07655d4e3d6bea4f16f11c076b55f7b
Nathan Leach <nathan.leach@checkmarx.com>
2020-05-27 07:23:00 -0500

    Initial add of mongodb output component

---
+++ MongoDBOutput/MongoDBOut.cs
@@ -0,0 +1,36 @@
+﻿using CxAnalytix.TransformLogic;
+using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using CxAnalytix.Configuration;
+
+namespace CxAnalytix.Out.MongoDBOutput
+{
+    internal sealed class MongoDBOut : IOutput
+    {
+        public String CollectionName { get; private set; }
+
+
+        public MongoDBOut(String collectionName)
+        {
+            CollectionName = collectionName;
+        }
+
+        public void write(IDictionary<string, string> record)
+        {
+
+            var cfg = Config.GetConfig<MongoOutConfig>(MongoOutConfig.SECTION_NAME);
+
+            var client = new MongoClient(cfg.ConnectionString);
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<BsonDocument>(CollectionName);
+
+            object dt = DateTime.Now;
+            BsonValue bv = BsonValue.Create(dt);
+
+            collection.InsertOne(new BsonDocument(new BsonDocument("DateStamp", bv)));
+
+        }
+    }
+}
---
+++ MongoDBOutput/MongoDBOutFactory.cs
@@ -0,0 +1,31 @@
+﻿using CxAnalytix.TransformLogic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CxAnalytix.Out.MongoDBOutput
+{
+    public sealed class MongoDBOutFactory : IOutputFactory
+    {
+        public IOutput newInstance(string recordType)
+        {
+            /*
+             * Configs:
+             * 
+             * connection string (mongodb://localhost:27017)
+             * database name
+             * 
+             * 
+             * the collections are based on the record name set up in the service.  
+             * 
+             * might need a tool to create the schema so that the indexes are created properly.
+             * should be able to override the collection names, then the collection names are set in the service config
+             * 
+             * need to refactor to allow String, Object dictionary.
+             * 
+            */
+
+            return new MongoDBOut(recordType);
+        }
+    }
+}
