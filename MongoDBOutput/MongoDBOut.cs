1a401c48d34273e1b77adf5fe6e87ddcb29f1175
Nathan Leach <nathan.leach@checkmarx.com>
2020-05-27 13:40:07 -0500

    Simple initial implementation of mongo writes

--- MongoDBOutput/MongoDBOut.cs
+++ MongoDBOutput/MongoDBOut.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using CxAnalytix.Configuration;
+using log4net;
 
 namespace CxAnalytix.Out.MongoDBOutput
 {
@@ -11,26 +12,38 @@ namespace CxAnalytix.Out.MongoDBOutput
     {
         public String CollectionName { get; private set; }
 
+        private static ILog _log = LogManager.GetLogger(typeof(MongoDBOut));
+
 
         public MongoDBOut(String collectionName)
         {
             CollectionName = collectionName;
         }
 
+        private BsonDocument BsonSerialize (IDictionary<string, object> record)
+        {
+            BsonDocument retVal = new BsonDocument();
+
+            foreach (var key in record.Keys)
+            {
+                retVal.Add(key.Replace ('.', '-'), BsonValue.Create(record[key]));
+            }
+
+
+            return retVal;
+        }
+
+
+
         public void write(IDictionary<string, object> record)
         {
-
             var cfg = Config.GetConfig<MongoOutConfig>(MongoOutConfig.SECTION_NAME);
 
             var client = new MongoClient(cfg.ConnectionString);
             var database = client.GetDatabase("test");
             var collection = database.GetCollection<BsonDocument>(CollectionName);
 
-            object dt = DateTime.Now;
-            BsonValue bv = BsonValue.Create(dt);
-
-            collection.InsertOne(new BsonDocument(new BsonDocument("DateStamp", bv)));
-
+            collection.InsertOne(BsonSerialize(record));
         }
     }
 }
