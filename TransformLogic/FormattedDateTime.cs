802e2c34651d9b7a0283fbb10dfef0a50f8ebaab
Nathan Leach <nathan.leach@checkmarx.com>
2020-05-27 07:40:11 -0500

    Initial impl of DateTime wrapper

---
+++ TransformLogic/FormattedDateTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CxAnalytix.TransformLogic
+{
+    public class FormattedDateTime
+    {
+        private static readonly String DATE_FORMAT = "yyyy-MM-ddTHH:mm:ss.fffzzz";
+
+        private DateTime _theDateTime;
+
+        public FormattedDateTime (DateTime dt)
+        {
+            _theDateTime = dt;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return _theDateTime.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _theDateTime.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return _theDateTime.ToString(DATE_FORMAT);
+        }
+    }
+}
