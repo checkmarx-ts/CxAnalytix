80746c8d7dd2ebb33dd10e9c5c5196c4a1f2b653
nleach999 <nleach999@gmail.com>
2021-04-07 13:26:19 -0500

    namespace re-org

--- CxRestClient/MNO/CxMnoPolicies.cs
+++ CxRestClient/MNO/CxMnoPolicies.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading;
 using CxRestClient.Utility;
+using CxRestClient.MNO.dto;
 
 namespace CxRestClient.MNO
 {
--- CxRestClient/MNO/CxMnoRetreivePolicyViolations.cs
+++ CxRestClient/MNO/CxMnoRetreivePolicyViolations.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Net.Http;
 using System.Threading;
 using CxRestClient.Utility;
+using CxRestClient.MNO.dto;
 
 namespace CxRestClient.MNO
 {
