d1d2ead6c3d55d393afd8c61150bea150129392a
Nathan Leach <nleach999@gmail.com>
2022-09-23 09:31:42 -0500

    begin cxone integration

---
+++ XForm/CxOneTransformer/Transformer.cs
@@ -0,0 +1,23 @@
+﻿using CxAnalytix.XForm.Common;
+using log4net;
+
+namespace CxAnalytix.XForm.CxOneTransformer
+{
+    public class Transformer : BaseTransformer
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Transformer));
+        private static readonly String STATE_STORAGE_FILE = "CxAnalytixExportState_CxOne.json";
+        private static readonly String MODULE_NAME = "CxOne";
+
+        public Transformer() : base(MODULE_NAME, typeof(Transformer), STATE_STORAGE_FILE)
+        {
+        }
+
+        public override string DisplayName => "CheckmarxOne";
+
+        public override void DoTransform(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
