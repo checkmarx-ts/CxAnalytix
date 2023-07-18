0178d82eb53f3039ad641cbd3113de2ba4e48f93
Nathan Leach <nleach999@gmail.com>
2023-07-18 09:07:05 -0500

    cxone crawl stability

---
+++ Libs/Exceptions/ScanCrawlException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CxAnalytix.Exceptions
+{
+    public class ScanCrawlException : Exception
+    {
+        private static String MakeMessage(String scanId, String projectName, String teamName) =>
+            $"Exception caught processing scan [{scanId}] in [{projectName}]" +
+                                        (!String.IsNullOrEmpty(teamName) ? $" assigned to team(s) [{teamName}]" : "");
+
+
+        public ScanCrawlException (String scanId, String projectName, String teamName) 
+            : base(ScanCrawlException.MakeMessage(scanId, projectName, teamName) )
+        {
+
+        }
+        public ScanCrawlException(String scanId, String projectName, String teamName, Exception ex) 
+            : base(ScanCrawlException.MakeMessage(scanId, projectName, teamName), ex)
+        {
+
+        }
+    }
+}
