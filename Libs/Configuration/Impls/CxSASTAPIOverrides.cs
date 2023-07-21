ac7d3117e4a31e5d394c84fcb3174966924c4b12
Nathan Leach <nleach999@gmail.com>
2023-07-21 08:51:51 -0500

    enable odata api

---
+++ Libs/Configuration/Impls/CxSASTAPIOverrides.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CxAnalytix.Configuration.Impls
+{
+    public sealed class CxSASTAPIOverrides : ConfigurationElement
+    {
+
+        [ConfigurationProperty("Project", IsRequired = false, DefaultValue = false)]
+        public bool Project
+        {
+            get => (bool)this["Project"];
+            set => this["Project"] = value;
+        }
+    }
+}
